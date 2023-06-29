dfc30fc45e346f69c4f8462119efab5ef5fdea90
pawelpaszki <pawelpaszki@gmail.com>
2023-06-29 14:51:01 +0100

    feat: styling of cards and add youtube link

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,6 +12,8 @@ import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Tooltip from '@mui/material/Tooltip';
+import DownloadIcon from '@mui/icons-material/Download';
+import YouTubeIcon from '@mui/icons-material/YouTube';
 
 const App = () => {
   const [query, setQuery] = React.useState("");
@@ -21,17 +23,13 @@ const App = () => {
   const search = () => {
     setLoaded(false);
     getSearchResults(query).then(result => {
-      // setMovies(result);
-      console.log(result);
       setResults(result);
-      console.log(result);
       setLoaded(true);
     })
   }
 
   const handleSearchQueryChange = (event) => {
     setQuery(event.target.value);
-    console.log(query);
   }
 
   return (
@@ -68,44 +66,67 @@ const App = () => {
             sx={{
               backgroundColor: 'black',
             }}>
-            {results.map((r, index) => (
+            {results.map((r) => (
               <Grid key={r.id} item xs={12} sm={6} md={4} lg={3} xl={3}>
                 <Tooltip title={r.title} placement="top">
 
-                <Card sx={{padding: '0.3rem'}}>
-                  <CardMedia
-                    image={r.thumbnails.high.url}
-                    title="green iguana"
-                    sx={{ height: 360 }}
-                  />
-                  <CardContent>
-                    <Typography gutterBottom variant="h5" component="div"
+                  <Card sx={{
+                    padding: '0.3rem',
+                    backgroundColor: "#272727"
+                  }}>
+                    <CardMedia
+                      image={r.thumbnails.high.url}
+                      title="green iguana"
                       sx={{
-                        display: '-webkit-box',
-                        overflow: 'hidden',
-                        textOverflow: 'ellipsis',
-                        WebkitBoxOrient: 'vertical',
-                        WebkitLineClamp: 1,
+                        height: 360
                       }}
-                    >
-                      {r.title}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary"
-                      sx={{
-                        display: '-webkit-box',
-                        overflow: 'hidden',
-                        textOverflow: 'ellipsis',
-                        WebkitBoxOrient: 'vertical',
-                        WebkitLineClamp: 2,
-                      }}
-                    >
-                      {r.description}
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small">Download</Button>
-                  </CardActions>
-                </Card>
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div"
+                        sx={{
+                          display: '-webkit-box',
+                          overflow: 'hidden',
+                          textOverflow: 'ellipsis',
+                          WebkitBoxOrient: 'vertical',
+                          WebkitLineClamp: 1,
+                          color: '#ff652f'
+                        }}
+                      >
+                        {r.title}
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary"
+                        sx={{
+                          display: '-webkit-box',
+                          overflow: 'hidden',
+                          textOverflow: 'ellipsis',
+                          WebkitBoxOrient: 'vertical',
+                          WebkitLineClamp: 2,
+                          color: '#14a76c'
+                        }}
+                      >
+                        {r.description}
+                      </Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Button size="medium" variant="outlined"
+                        sx={{
+                          color: '#ffe400',
+                          marginRight: '1rem'
+                        }}
+                        endIcon={<DownloadIcon />}
+                      >
+                        Download
+                      </Button>
+                      <Button size="medium" variant="outlined" href={r.link} target="_blank"
+                        sx={{
+                          color: 'red'
+                        }}
+                        endIcon={<YouTubeIcon />}
+                      >
+                        See on YT
+                      </Button>
+                    </CardActions>
+                  </Card>
                 </Tooltip>
               </Grid>
             ))}
